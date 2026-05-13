d0b23a30937859cb4d7eecd483d3fa73dd7da667
silviacpmachado <silviacpmachado@gmail.com>
2026-05-12 21:23:21 -0300

    inicializa

---
+++ aula01/comandos.py
@@ -0,0 +1,12 @@
+# python -m venv venv
+# source ./venv/Scripts/activate
+# Instalar as bibliotecas:
+# pip install pandas .......
+# deactivate - desativa
+# rm -rf venv - apaga a pasta de venv
+
+# Usar o arquivo requeriments p/ fazer a instalação das bibliotecas
+# pythom -m venv venv 
+# pip install -r requirements.txt
+
+# nao sobe para o github a pasta venv.
